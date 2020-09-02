0bc65c5345c7fb1be4be9070e41368175eaf6272
Paul Willard <paul@paulwillard.nz>
2020-09-03 10:54:47 +1200

    updated service worker

--- sw.js
+++ sw.js
@@ -2,52 +2,54 @@
 layout: blank
 ---
 
-importScripts("/assets/js/workbox-v3.6.2/workbox-sw.js");
+importScripts("/assets/js/workbox-v5.1.3/workbox-sw.js");
 
 workbox.setConfig({
   debug: false,
-  modulePathPrefix: "/assets/js/workbox-v3.6.2/"
+  modulePathPrefix: "/assets/js/workbox-v5.1.3/"
 });
 
-workbox.skipWaiting();
-workbox.clientsClaim();
-workbox.precaching.suppressWarnings();
-workbox.navigationPreload.enable();
-workbox.routing.setDefaultHandler(workbox.strategies.networkFirst());
-
+// precache all mmy posts and pages + bundles.js
 workbox.precaching.precacheAndRoute([
-{% for post in site.posts %}
-  {
-    url: "{{ post.url }}",
-    revision: "{{ "now" | date: "%s" }}"
-  },
+{% for post in site.posts %}{url: '{{ post.url }}', revision: '{{ "now" | date: "%s" }}' },
 {% endfor %}
-{% for page in site.pages %}
-  {% if page.url %}
-  {
-    url: "{{ page.url }}",
-    revision: "{{ "now" | date: "%s" }}"
-  },
-  {% endif %}
+{% for page in site.pages %}{% if page.url %}{url: '{{ page.url }}', revision: '{{ "now" | date: "%s" }}' },{% endif %}
 {% endfor %}
-  {
-    url: "/assets/js/bundle.js",
-    revision: "{{ "now" | date: "%s" }}"
-  }
-]);
+{url: '/assets/js/bundle.js', revision: '{{ "now" | date: "%s" }}' }
+], {
+  directoryIndex: null,
+});
 
+// cache images
 workbox.routing.registerRoute(
-  /(.*)/,
-  workbox.strategies.networkFirst({
-    cacheName: "{{ site.site_name | slugify }}-cache-v1",
+  ({request}) => request.destination === 'image',
+  new workbox.strategies.CacheFirst({
+    cacheName: '{{ site.site_name | slugify }}-cache-v1',
     plugins: [
-      new workbox.expiration.Plugin({
+      new workbox.expiration.ExpirationPlugin({
         maxEntries: 60,
         maxAgeSeconds: 10 * 24 * 60 * 60 // 10 Days
       }),
-      new workbox.cacheableResponse.Plugin({
+      new workbox.cacheableResponse.CacheableResponsePlugin({
         statuses: [0, 200]
-      })
-    ]
+      }),
+    ],
+  })
+);
+
+// cache styles
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'style',
+  new workbox.strategies.CacheFirst({
+    cacheName: '{{ site.site_name | slugify }}-cache-v1',
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 10 * 24 * 60 * 60 // 10 Days
+      }),
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200]
+      }),
+    ],
   })
 );
