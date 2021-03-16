4786f7b3bcec8f0655aeb83659247da1eb895aa9
Paul Willard <paul@paulwillard.nz>
2021-03-17 12:13:48 +1300

    updated workbox

--- sw.js
+++ sw.js
@@ -2,20 +2,20 @@
 layout: blank
 ---
 
-importScripts("/assets/js/workbox-v5.1.3/workbox-sw.js");
+importScripts("/assets/js/workbox-v6.1.2/workbox-sw.js");
 
 workbox.setConfig({
   debug: false,
-  modulePathPrefix: "/assets/js/workbox-v5.1.3/"
+  modulePathPrefix: "/assets/js/workbox-v6.1.2/"
 });
+
+var CACHE_NAME='{{ site.site_name | slugify }}-cache-v1';
 
 // precache all mmy posts and pages + bundles.js
 workbox.precaching.precacheAndRoute([
 {% for post in site.posts %}{url: '{{ post.url }}', revision: '{{ "now" | date: "%s" }}' },
-{% endfor %}
-{% for page in site.pages %}{% if page.url %}{url: '{{ page.url }}', revision: '{{ "now" | date: "%s" }}' },{% endif %}
-{% endfor %}
-{url: '/assets/js/bundle.js', revision: '{{ "now" | date: "%s" }}' }
+{% endfor %}{% for page in site.pages %}{% if page.url %}{url: '{{ page.url }}', revision: '{{ "now" | date: "%s" }}' },{% endif %}
+{% endfor %}{url: '/assets/js/bundle.js', revision: '{{ "now" | date: "%s" }}' }
 ], {
   directoryIndex: null,
 });
@@ -24,7 +24,7 @@ workbox.precaching.precacheAndRoute([
 workbox.routing.registerRoute(
   ({request}) => request.destination === 'image',
   new workbox.strategies.CacheFirst({
-    cacheName: '{{ site.site_name | slugify }}-cache-v1',
+    cacheName: CACHE_NAME,
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 60,
@@ -41,7 +41,7 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
   ({request}) => request.destination === 'style',
   new workbox.strategies.CacheFirst({
-    cacheName: '{{ site.site_name | slugify }}-cache-v1',
+    cacheName: CACHE_NAME,
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 60,
@@ -53,3 +53,54 @@ workbox.routing.registerRoute(
     ],
   })
 );
+
+var urlsToCache = [
+{% for post in site.posts %}  '{{ post.url }}',
+{% endfor %}{% for page in site.pages %}{% if page.url %}  '{{ page.url }}',{% endif %}
+{% endfor %}  '/assets/js/bundle.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
